f549369a0806f2ba3a5cf2ffa8d5a6d3d2da6b6b
Kaizhe Huang <derek0405@gmail.com>
2019-05-28 12:11:39 -0700

    image scanning admission controller: set ANCHORE_CLI_PASS internally

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,10 +12,7 @@ if [[ -z "$ANCHORE_CLI_USER" ]]; then
 	exit 1;
 fi;
 
-if [[ -z "${ANCHORE_CLI_PASS+x}" ]]; then
-	echo '${ANCHORE_CLI_PASS} is unset';
-	exit 1;
-fi;
+export ANCHORE_CLI_PASS=""
 
 sed 's@{{ANCHORE_CLI_URL}}@'"${ANCHORE_CLI_URL}"'@; s@{{ANCHORE_CLI_TOKEN}}@'"${ANCHORE_CLI_USER}"'@' image-scanning-admission-controller.yaml | kubectl apply -f -
 
