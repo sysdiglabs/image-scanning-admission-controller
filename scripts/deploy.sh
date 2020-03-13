53f50551d9a87013e375d2b64597d06f6cfc51d8
kaizhe <derek0405@gmail.com>
2020-03-13 11:21:16 -0700

    extend sleep to 15 sec in deploy
    Signed-off-by: kaizhe <derek0405@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,7 @@ export ANCHORE_CLI_PASS=""
 sed 's@{{ANCHORE_CLI_URL}}@'"${ANCHORE_CLI_URL}"'@; s@{{ANCHORE_CLI_TOKEN}}@'"${ANCHORE_CLI_USER}"'@' image-scanning-admission-controller.yaml | kubectl apply -f -
 
 set -ex
-sleep 10
+sleep 15
 kubectl get all -n image-scan-k8s-webhook-system
 
 CA_BUNDLE=$(kubectl -n image-scan-k8s-webhook-system get secret image-scan-k8s-webhook-webhook-server-secret -o jsonpath='{.data.ca-cert\.pem}')
